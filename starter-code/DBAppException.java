573828d653376229771f4bf4119fec8cc0090a81
youssef <youssefmmagdy55@gmail.com>
2024-02-26 17:12:08 +0200

    ..

---
+++ starter-code/DBAppException.java
@@ -0,0 +1,14 @@
+
+/** * @author Wael Abouelsaadat */ 
+
+public class DBAppException extends Exception {
+
+
+	public DBAppException( String strMessage ){
+		super( strMessage );
+	}
+	
+	public static void main(String[] args) {
+		System.out.print(3);
+	}
+}
